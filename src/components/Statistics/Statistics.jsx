5cb4df5eb04a0e43befb2e8a2bc1fdeec5ccd160
MadDog83 <130376768+MadDog83@users.noreply.github.com>
2023-11-22 23:32:11 +0100

    add styles statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import './Statistics.css';
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 const Statistics = ({ title, stats }) => (
   <section className="statistics">
     {title && <h2 className="title">{title}</h2>}
 
     <ul className="stat-list">
       {stats.map((stat) => (
-        <li className="item" key={stat.id}>
+        <li className="item" key={stat.id} style={{ backgroundColor: getRandomColor() }}>
           <span className="label">{stat.label}</span>
           <span className="percentage">{stat.percentage}%</span>
         </li>
